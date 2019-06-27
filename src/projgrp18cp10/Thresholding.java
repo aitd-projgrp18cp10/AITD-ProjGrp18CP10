2404e0a766fbd8c308163ede4e10a11f7301b1cd
Chinmay P <chinmayp79@users.noreply.github.com>
2019-06-27 17:48:06 +0530

    Fix

--- src/projgrp18cp10/Thresholding.java
+++ src/projgrp18cp10/Thresholding.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projgrp18cp10;
 
 import static java.lang.Math.round;
