1d2f091bc8ffe9c0cda92830b78501434e69ec74
Chinmay P <chinmayp79@users.noreply.github.com>
2019-06-27 17:47:40 +0530

    Classification by Amruta

--- src/projgrp18cp10/Features.java
+++ src/projgrp18cp10/Features.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projgrp18cp10;
 
 /**
  *
- * @author ProjGrp18CP10 , Chinmay P., Kalpita
+ * @author ProjGrp18CP10 : Chinmay , Kalpita , Amruta
  */
 public class Features
 {
@@ -195,5 +190,92 @@ public class Features
         pdb.psnr = psnr;
     }
     
-	
+    public static void Classify(ProjDataBase pdb)
+    {
+        double asm = pdb.asm;
+        double correlation = pdb.correlation;
+        double energy = pdb.energy;
+        double homogeneity = pdb.homogeneity;
+        double contrast = pdb.contrast;
+        double idm = pdb.idm;
+        
+        // ASM
+        if (asm >= 0 && asm <= 0.00025)
+            pdb.Lasm = "1";
+        else if (asm > 0.00025 && asm <= 0.0005)
+            pdb.Lasm = "2";
+        else if (asm > 0.0005 && asm <= 0.00075)
+            pdb.Lasm = "3";
+        else if (asm > 0.00075 && asm <= 0.0001)
+            pdb.Lasm = "4";
+        
+        // Correlation
+        if (correlation >0 && correlation <0.9 )  
+            pdb.Lcorrelation = "2";
+        else if (correlation >=0.9 && correlation <= 1) 
+            pdb.Lcorrelation = "1";
+        
+        // Energy
+        if (energy >= 0 && energy <= 0.020)
+            pdb.Lenergy = "1";
+        else if (energy > 0.020 && energy <= 0.040)
+            pdb.Lenergy = "2";
+        else if (energy > 0.040 && energy <= 0.060)
+            pdb.Lenergy = "3";
+        else if (energy > 0.060 && energy <= 0.080)
+            pdb.Lenergy = "4";
+        else if (energy > 0.080 && energy <= 0.1)
+            pdb.Lenergy = "5";
+        else if (energy > 0.1 && energy <= 0.12)
+            pdb.Lenergy = "6";
+        
+        // Homogeneity
+        if (homogeneity >= 0 && homogeneity <= 0.10)
+            pdb.Lhomogeneity = "c";
+        else if (homogeneity > 0.10 && homogeneity <= 0.20)
+            pdb.Lhomogeneity = "B";
+        else if (homogeneity > 0.20 && homogeneity <= 0.44)
+            pdb.Lhomogeneity = "A";
+        
+        // Contrast
+        if (contrast >= 0 && contrast <= 45) 
+            pdb.Lcontrast = "1";             
+        else if (contrast > 45 && contrast <= 90)
+            pdb.Lcontrast = "2";
+        else if (contrast > 90 && contrast <= 135)
+            pdb.Lcontrast = "3";
+        else if (contrast > 90 && contrast <= 135)
+            pdb.Lcontrast = "4";
+        else if (contrast > 90 && contrast <= 135)
+            pdb.Lcontrast = "5";
+        else if (contrast > 90 && contrast <= 135)
+            pdb.Lcontrast = "6";
+        else if (contrast > 90 && contrast <= 135)
+            pdb.Lcontrast = "7";
+        else if (contrast > 90 && contrast <= 135)
+            pdb.Lcontrast = "8";
+        else if (contrast > 90 && contrast <= 135)
+            pdb.Lcontrast = "9";
+        else if (contrast > 90 && contrast <= 135)
+            pdb.Lcontrast = "10";
+        
+        // IDM
+        if (idm >= 0 && idm <= 0.20)
+            pdb.Lidm = "2";
+        else if (idm > 0.20 && idm <= 0.40)
+            pdb.Lidm = "1"; 
+    }
+    
+    public static String testFunction(String ASM,String Correlation,String Energy,String Homogeneity,String Contrast,String IDM) 
+    {
+        String Decision="";
+        if(Homogeneity == "c")
+            Decision="Eczema";
+        else if(Homogeneity == "B")
+            Decision="Melanoma";
+        else if(Homogeneity == "A")
+            Decision="Healthy";
+        return Decision;
+    }
+
 }
